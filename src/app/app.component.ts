28158e7a6bbcb9b31ce6d4ccdc0718ee5c9a79b9
alarabyy <137658373+alarabyy@users.noreply.github.com>
2025-07-07 14:58:40 +0300

    fix: Cor3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
 import { CommonModule } from '@angular/common';
-import { FooterComponent } from "./components/footer/footer.component";
 import { FloatingControlsComponent } from "./components/floating-controls/floating-controls.component";
 import { LoaderComponent } from "./components/loader/loader.component";
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [CommonModule ,HttpClientModule ,FormsModule , RouterOutlet, NavComponent, CommonModule, FooterComponent, FloatingControlsComponent, LoaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
