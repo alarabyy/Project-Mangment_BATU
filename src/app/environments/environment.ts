dd0fb32ae04aa97f611e495a896737ed88c2b1df
Mohamed Gaber <63926336+DevMohamedGaber@users.noreply.github.com>
2025-10-03 00:43:25 +0300

    Fix document base URL in environment configuration by adding a trailing slash for consistency with the uploads directory.

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   production: false,
   apiUrl: 'https://batuprojects.runasp.net',
 
-  docBaseUrl: 'https://batuprojects.runasp.net/uploads',
+  docBaseUrl: 'https://batuprojects.runasp.net/uploads/',
   geminiApiKey: 'AIzaSyA5uUncE_p0rXg9tsOFWR23O9g4chWccHw',
   imageBaseUrl: 'https://batuprojects.runasp.net/uploads/', // **تأكد أن هذا المسار يطابق مجلد الـ `uploads` في الـ Backend**
   imagesBaseUrl: 'https://batuprojects.runasp.net/uploads/',
