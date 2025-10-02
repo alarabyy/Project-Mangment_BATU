64bd9553a3afc3357bd31a29f415dcf418afe3d0
Mohamed Gaber <63926336+DevMohamedGaber@users.noreply.github.com>
2025-10-03 00:42:15 +0300

    Update environment configuration to change document and image base URLs to the uploads directory for consistency with backend structure.

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -3,9 +3,9 @@ export const environment = {
   production: false,
   apiUrl: 'https://batuprojects.runasp.net',
 
-  docBaseUrl: 'https://batuprojects.runasp.net/api/project',
+  docBaseUrl: 'https://batuprojects.runasp.net/uploads',
   geminiApiKey: 'AIzaSyA5uUncE_p0rXg9tsOFWR23O9g4chWccHw',
   imageBaseUrl: 'https://batuprojects.runasp.net/uploads/', // **تأكد أن هذا المسار يطابق مجلد الـ `uploads` في الـ Backend**
-  imagesBaseUrl: 'https://batuprojects.runasp.net/images/',
+  imagesBaseUrl: 'https://batuprojects.runasp.net/uploads/',
   apiUploadUrl: 'https://batuprojects.runasp.net/uploads/',
 };
